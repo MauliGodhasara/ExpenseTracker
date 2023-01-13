3d25000561fdbb62c23df8be4bf75632567cf661
Mauli Godhasara <122619622+MauliGodhasara@users.noreply.github.com>
2023-01-13 21:45:06 +0530

    Add files via upload

---
+++ Migrations/20221220105421_Initial Create.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTracker.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categoryDetails",
+                columns: table => new
+                {
+                    Categoryid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Expenseslimet = table.Column<string>(type: "nvarchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categoryDetails", x => x.Categoryid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "expensesDetails",
+                columns: table => new
+                {
+                    Expensesid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Categoryid = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Amount = table.Column<string>(type: "nvarchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_expensesDetails", x => x.Expensesid);
+                    table.ForeignKey(
+                        name: "FK_expensesDetails_categoryDetails_Categoryid",
+                        column: x => x.Categoryid,
+                        principalTable: "categoryDetails",
+                        principalColumn: "Categoryid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_expensesDetails_Categoryid",
+                table: "expensesDetails",
+                column: "Categoryid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "expensesDetails");
+
+            migrationBuilder.DropTable(
+                name: "categoryDetails");
+        }
+    }
+}
